ea4c6414f9dec040cd0e2cf17f3e19d89661e805
shahid <shahdbest@gmail.com>
2022-08-19 16:08:46 +0530

    a

---
+++ src/main/java/com/stackroute/exercises/StringSort.java
@@ -0,0 +1,13 @@
+package com.stackroute.exercises;
+
+
+import java.util.List;
+
+public class StringSort {
+
+    //write here logic to sort a string List
+    public String stringSorter(List<String> stringList, String sortingOrder) {
+
+        return null;
+    }
+}
